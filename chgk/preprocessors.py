4ffec5b96d45eb2068cc696f96fde7b071fe49de
Svelval <svelval2015@gmail.com>
2024-05-29 14:36:13 +0500

    add startup & shutdown preprocessors

--- chgk/preprocessors.py
+++ chgk/preprocessors.py
@@ -1,4 +1,13 @@
 from chgk.context_processor import static_files_context_processor
+from settings import db
+
+
+async def on_startup():
+    await db.create_connection_pool()
+
+
+async def on_shutdown():
+    await db.close_all_connections()
 
 
 async def context_processor():
